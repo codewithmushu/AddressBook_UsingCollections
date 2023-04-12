83f1d79f4fbfb36cb4fd1376bc084f9f6ab80d33
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-12 17:02:21 +0530

    [Muskan] Added : C# program to add person and search person by name methods

---
+++ AddressBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_Collection
+{
+    public class AddressBook
+    {
+        private List<Person> people;
+
+        public AddressBook()
+        {
+            people = new List<Person>();
+        }
+
+        public void AddPerson(Person person)
+        {
+            if (people.Contains(person))
+            {
+                Console.WriteLine($"Person with name {person.Name} already exists in the address book.");
+            }
+            else
+            {
+                people.Add(person);
+            }
+        }
+
+        public List<Person> SearchByName(string name)
+        {
+            return people.Where(person => person.Name == name).ToList();
+        }
+
+    }
+}
