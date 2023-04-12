43ba5a35d012e7de94d02ff890f64d88ace83c74
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-12 17:00:25 +0530

    [Muskan] Added : Created constructor for name, email address and phone number

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_Collection
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Person person && Name == person.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
