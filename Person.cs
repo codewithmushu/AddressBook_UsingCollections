3ff6ba94b89981f1c8c4a93b4cd11039f3f181fd
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-13 00:37:57 +0530

    [Muskan] Added : Define City and State for Address Book.

--- Person.cs
+++ Person.cs
@@ -11,6 +11,8 @@ namespace AddressBook_Collection
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
 
         public override bool Equals(object obj)
         {
