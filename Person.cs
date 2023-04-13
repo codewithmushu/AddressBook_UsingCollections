5eda71064eeb65189840db4cb5447e3986bd7257
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-13 12:30:03 +0530

    [Muskan] Added : Define persons data

--- Person.cs
+++ Person.cs
@@ -13,13 +13,12 @@ namespace AddressBook_Collection
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string ZipCode { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
 
         public override string ToString()
         {
-            return $"{FirstName} {LastName} ({PhoneNumber}), {Address}, {City}, {State} {ZipCode}, {Email}";
+            return $"{FirstName} {LastName} ({PhoneNumber}), {Address}, {City}, {State} , {Email}";
         }
     }
 }
