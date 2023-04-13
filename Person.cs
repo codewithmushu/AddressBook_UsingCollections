9369889c59eb07c4a7e9226666abc8467b6cf842
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-13 11:55:00 +0530

    [Muskan] Added : Define all the parameters and get Hashcode.

--- Person.cs
+++ Person.cs
@@ -8,15 +8,31 @@ namespace AddressBook_Collection
 {
     public class Person
     {
-        public string Name { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+        public string Zip { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
 
         public override bool Equals(object obj)
         {
-            return obj is Person person && Name == person.Name;
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Person other = (Person)obj;
+
+            return Name == other.Name;
         }
 
         public override int GetHashCode()
